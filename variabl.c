db1b06a86b2b025d4cdb50c37269915ee98cf776
storytellersatya <93367707+storytellersatya@users.noreply.github.com>
2021-10-28 23:51:20 -0700

    Add files via upload

---
+++ variabl.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	//variable declaration
+	int age;
+	float cgpa;
+	char initial;
+	
+	//initialization
+	age = 17;
+	cgpa = 8.0;
+	initial = 'm';
+	
+	//output
+	printf("%d\n", age);
+	printf("%f\n", cgpa);
+	printf("%c", initial);
+	
+
+}
